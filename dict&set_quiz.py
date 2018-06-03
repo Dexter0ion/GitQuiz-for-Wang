a950e413e130d59a2a6f219fe36c055610bc2e83
邵博超DEXION <dexter0ion@gmail.com>
2018-06-03 20:05:41 +0800

    字典与集合测试

---
+++ dict&set_quiz.py
@@ -0,0 +1,40 @@
+'''
+Python3 tutorial
+Author：Dexter0ion
+Mail：dexter0ion@gmail.com
+'''
+
+#假设现有菜品和售价两个列表，请用此二列表构建字典（dict）nenu
+foods = ['RostDuc','HotPot','Steak','Hamburger']
+prices = ['64','59','60','83']
+menu = {}
+
+#your code below:
+
+
+
+#使用print输出先前创建的menu字典
+
+
+#向菜单新添加一组键-值 'crayfish':98
+
+
+#删除菜单dict中的Steak一项
+
+
+#输出目前menu键-值数
+
+
+#现有两集合(set)
+s1 = set([1,2,3,6,7,8,5])
+s2 = set([3,2,1,4,1,2,3,1,21,12])
+
+#请输出集合s1和s2的交集
+
+
+#请输出集合s1和s2的并集
+
+
+
+
+
