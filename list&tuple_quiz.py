7790aa8b79fdf5c750a4e002aa8f457591fbc5dd
邵博超DEXION <dexter0ion@gmail.com>
2018-06-03 20:05:00 +0800

    列表与元组测试

---
+++ list&tuple_quiz.py
@@ -0,0 +1,32 @@
+'''
+Python3 tutorial
+Author：Dexter0ion
+Mail：dexter0ion@gmail.com
+'''
+
+#现有一个存储菜品名称的list（列表）
+foods = ['RostDuck','HotPot','Steak','Hamburger']
+
+#输出foods list的元素个数
+
+
+#向列表foods末尾追加元素MashedPotato
+
+
+#向列表第二位插入元素CreamSoup
+
+
+#输出数组全部元素(直接使用print)
+
+
+
+#删除foods中元素Steak
+
+
+#输出数组全部元素(使用循环）
+
+
+#尝试创建一tuple（元组），名称为colors，元素为Aka，Ao以及Kuro
+
+
+#请写出list（列表）与tuple（元组）的异同
